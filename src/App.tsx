d8c36103bc586f5ae6eaf934db2e41c0ffff813a
akisha <akisha@qburst.com>
2026-02-03 13:20:16 +0530

    log added to test git merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -7,6 +7,10 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    console.log('log added to test git merge');
+  }, [count]);
+  
   return (
     <>
       <div>
