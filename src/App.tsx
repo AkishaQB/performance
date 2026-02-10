9f6af5702e66d0ee32641be76aa21356cb727b40
akisha <akisha@qburst.com>
2026-02-10 16:29:21 +0530

    chore: update search-sdk dependency to version 0.1.5 and implement search functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { SearchClient } from '@akisha_jm/search-sdk';
 function App() {
+
+  const searchUrl = 'https://dummyjson.com/products/search'
+  
+const search = SearchClient({
+  debounceMs: 300,
+  maxCalls: 5,
+  interval: 1000,
+  cacheTtl: 10_000
+});
+  
+  const onSearchInput = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+  try {
+    const value = event.currentTarget.value;
+    const results = await search(searchUrl, value);
+    console.log(results);
+  } catch (err) {
+    console.error("Search failed:", err);
+  }
+}
+
   return (
     <main>
       <h1>Search</h1>
 
       <form>
         <label htmlFor="search">Search</label>
-        <input id="search" type="search" placeholder="Type to search..." />
+        <input id="search" type="search" placeholder="Type to search..." onKeyDown={onSearchInput} />
       </form>
 
       <section>
